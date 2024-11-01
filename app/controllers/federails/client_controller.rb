5a8816a38c54b4f81b5d548a7ae70f103ad8fa4e
Manuel Tancoigne <manu@experimentslabs.com>
2024-11-01 14:02:32 +0100

    feat(client): Enforce authorization calls in controllers

--- app/controllers/federails/client_controller.rb
+++ app/controllers/federails/client_controller.rb
@@ -2,6 +2,8 @@ module Federails
   class ClientController < Federails.configuration.base_client_controller.constantize
     include Pundit::Authorization
 
+    after_action :verify_authorized
+
     layout Federails.configuration.app_layout if Federails.configuration.app_layout
   end
 end
