883c4e0ffe360d9b6ed520b4188f453f6ab23974
Manuel Tancoigne <manu@experimentslabs.com>
2024-12-15 14:41:54 +0100

    feat(dummy): Configure Post for federails

--- spec/dummy/app/models/post.rb
+++ spec/dummy/app/models/post.rb
@@ -1,7 +1,30 @@
 class Post < ApplicationRecord
+  include Federails::DataEntity
+
+  acts_as_federails_data handles:             'Note',
+                         actor_entity_method: :user
+
   validates :title, presence: true, allow_blank: false
   validates :content, presence: true, allow_blank: false
 
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :comments
+
+  def to_activitypub_object
+    Federails::DataTransformer::Note.to_federation self,
+                                                   name:    title,
+                                                   content: content
+  end
+
+  def self.handle_federated_object?(hash)
+    # Only "top level" notes should be saved as Post; replies are handled by Comment
+    hash['inReplyTo'].blank?
+  end
+
+  def self.from_activitypub_object(hash)
+    Federails::Utils::Object.timestamp_attributes(hash)
+                            .merge federated_url: hash['id'],
+                                   title:         hash['published'] || 'A post',
+                                   content:       hash['content']
+  end
 end
