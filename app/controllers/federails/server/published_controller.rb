74d06bed0528054ed32702619d0824224b612818
James Smith <james@floppy.org.uk>
2025-01-14 14:09:35 +0000

    feat(data): Add url_param option to acts_as_federails_data
    so we can use fields other than database row ID in URLs

--- app/controllers/federails/server/published_controller.rb
+++ app/controllers/federails/server/published_controller.rb
@@ -3,19 +3,27 @@ module Federails
     # Controller to render ActivityPub representation of entities configured with Federails::DataEntity
     class PublishedController < Federails::ServerController
       def show
-        @publishable = type_scope.find(params[:id])
+        @publishable = type_scope.find_by!(url_param => params[:id])
         authorize @publishable, policy_class: Federails::Server::PublishablePolicy
       end
 
       private
 
+      def publishable_config
+        return @publishable_config if instance_variable_defined? :@publishable_config
+
+        _, @publishable_config = Federails.configuration.data_types.find { |_, v| v[:route_path_segment].to_s == params[:publishable_type] }
+        raise ActiveRecord::RecordNotFound, "Invalid #{params[:publishable_type]} type" unless @publishable_config
+
+        @publishable_config
+      end
+
+      def url_param
+        publishable_config[:url_param]
+      end
+
       def type_scope
-        return @type_scope if instance_variable_defined? :@type_scope
-
-        _, config = Federails.configuration.data_types.find { |_, v| v[:route_path_segment].to_s == params[:publishable_type] }
-        raise ActiveRecord::RecordNotFound, "Invalid #{params[:publishable_type]} type" unless config
-
-        @type_scope = config[:class].all
+        publishable_config[:class].all
       end
     end
   end
