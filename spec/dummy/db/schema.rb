58e11556eb357129be1c50fc0d0418557ebcccbd
Manuel Tancoigne <manu@experimentslabs.com>
2025-03-01 15:12:53 +0100

    fix(db): Update schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_29_104734) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_01_082500) do
   create_table "comments", force: :cascade do |t|
     t.text "content", null: false
     t.integer "user_id"
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_29_104734) do
     t.string "uuid"
     t.text "public_key"
     t.text "private_key"
+    t.json "extensions"
     t.index ["entity_type", "entity_id"], name: "index_federails_actors_on_entity", unique: true
     t.index ["federated_url"], name: "index_federails_actors_on_federated_url", unique: true
     t.index ["uuid"], name: "index_federails_actors_on_uuid", unique: true
