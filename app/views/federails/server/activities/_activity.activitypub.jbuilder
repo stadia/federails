0cb3fab426ca1bbc93f082d8ca37c65c79dee724
James Smith <james@floppy.org.uk>
2025-04-01 14:28:18 +0100

    handle rendering of activities used as the object of other activities

--- app/views/federails/server/activities/_activity.activitypub.jbuilder
+++ app/views/federails/server/activities/_activity.activitypub.jbuilder
@@ -1,13 +1,18 @@
 context = true unless context == false
+addressing = true unless addressing == false
 json.set! '@context', 'https://www.w3.org/ns/activitystreams' if context
 
 json.id Federails::Engine.routes.url_helpers.server_actor_activity_url activity.actor, activity
 json.type activity.action
 json.actor activity.actor.federated_url
-json.to ['https://www.w3.org/ns/activitystreams#Public']
-json.cc [activity.actor.followers_url]
+if addressing
+  json.to ['https://www.w3.org/ns/activitystreams#Public']
+  json.cc [activity.actor.followers_url]
+end
 
-if activity.entity.respond_to? :to_activitypub_object
+if activity.entity.is_a? Federails::Activity
+  json.object { json.partial!('federails/server/activities/activity', activity: activity.entity, context: false, addressing: false) }
+elsif activity.entity.respond_to? :to_activitypub_object
   json.object activity.entity.to_activitypub_object
 elsif activity.entity.respond_to? :federated_url
   json.object activity.entity.federated_url
---
+++ spec/views/federails/server/activities/show_jbuilder_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe 'federails/server/activities/show', type: :view do
+  let(:local_actor) { FactoryBot.create :local_actor }
+  let(:distant_actor) { FactoryBot.create :distant_actor }
+
+  context 'when rendering a Follow activity' do
+    let(:follow) { FactoryBot.create :following, actor: local_actor, target_actor: distant_actor }
+    let(:json_result) do
+      assign(:activity, follow.follow_activity)
+      render
+      JSON.parse(rendered)
+    end
+
+    it 'has an id' do
+      expect(json_result['id']).to eq "http://localhost/federation/actors/#{local_actor.uuid}/activities/#{follow.follow_activity.uuid}"
+    end
+
+    it 'has Follow type' do
+      expect(json_result['type']).to eq 'Follow'
+    end
+
+    it 'references actor by URL' do
+      expect(json_result['actor']).to eq local_actor.federated_url
+    end
+
+    it 'references target actor by URL' do
+      expect(json_result['object']).to eq distant_actor.federated_url
+    end
+  end
+
+  context 'when rendering an Undo activity' do
+    let(:follow) { FactoryBot.create :following, actor: local_actor, target_actor: distant_actor }
+    let(:undo) do
+      follow.destroy
+      Federails::Activity.find_by action: 'Undo'
+    end
+    let(:json_result) do
+      assign(:activity, undo)
+      render
+      JSON.parse(rendered)
+    end
+
+    it 'has Undo type' do
+      expect(json_result['type']).to eq('Undo').and(eq(undo.action))
+    end
+
+    it 'references actor by URL' do
+      expect(json_result['actor']).to eq follow.actor.federated_url
+    end
+
+    it 'references includes original Follow as object' do
+      uuid = follow.follow_activity.uuid
+      expect(json_result['object']).to eq({
+                                            'id'     => "http://localhost/federation/actors/#{local_actor.uuid}/activities/#{uuid}",
+                                            'type'   => 'Follow',
+                                            'actor'  => local_actor.federated_url,
+                                            'object' => distant_actor.federated_url,
+                                          })
+    end
+  end
+end
