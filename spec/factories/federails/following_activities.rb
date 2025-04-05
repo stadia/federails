ec62d5f81bfeda190343697eba8ca6b2a6281ab9
Manuel Tancoigne <manu@experimentslabs.com>
2025-04-05 08:46:20 +0200

    fix(factories): Remove unused factory

--- spec/factories/federails/following_activities.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :following_activity, class: 'Federails::Activity' do
-    actor factory: [:distant_actor]
-    # Create the following and return the Create activity
-    initialize_with { create(:following, actor: actor).follow_activity }
-  end
-end
