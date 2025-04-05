d0d8e6b7c0545c2348503014323e6265546c28b3
Manuel Tancoigne <manu@experimentslabs.com>
2025-04-05 09:38:07 +0200

    chore(spec): Harmonize variables and complete Following specs

--- spec/models/federails/following_spec.rb
+++ spec/models/federails/following_spec.rb
@@ -4,106 +4,135 @@ require 'fediverse/notifier'
 module Federails
   RSpec.describe Following, type: :model do
     context 'with a follow relationship in place' do
-      let(:actor) { FactoryBot.create(:user).federails_actor }
-      let(:other_actor) { FactoryBot.create(:user).federails_actor }
-      let(:following) { described_class.create actor: actor, target_actor: other_actor }
+      let(:follower) { FactoryBot.create :local_actor }
+      let(:target) { FactoryBot.create :local_actor }
+
+      before do
+        described_class.create! actor: follower, target_actor: target
+      end
 
       it 'fails to create the same following twice' do
-        following
-        second = described_class.create actor: actor, target_actor: other_actor
-        expect(second).not_to be_valid
+        expect { described_class.create! actor: follower, target_actor: target }.to raise_error(/Target actor has already been taken/)
       end
     end
 
-    context 'when creating a follow' do
-      let(:user) { FactoryBot.create :user }
-      let(:target) { FactoryBot.create :user }
+    context 'when following a local actor' do
+      let(:follower) { FactoryBot.create :local_actor }
+      let(:target) { FactoryBot.create :local_actor }
+      let(:following) { described_class.build actor: follower, target_actor: target }
 
       it 'executes after_followed callback in target' do
         # accept_follow is set up as our callback method
-        allow(target).to receive(:accept_follow)
-        f = described_class.create actor: user.federails_actor, target_actor: target.federails_actor
-        expect(target).to have_received(:accept_follow).with(f).once
+        allow(target.entity).to receive(:accept_follow)
+        following.save!
+
+        expect(target.entity).to have_received(:accept_follow).with(following).once
       end
 
       it 'executes after_follow_accepted callback in target' do
+        following.save!
+
         # follow_accepted is set up as our callback method
-        allow(user).to receive(:follow_accepted)
-        f = described_class.create actor: user.federails_actor, target_actor: target.federails_actor
-        f.accept!
-        expect(user).to have_received(:follow_accepted).with(f).once
+        allow(follower.entity).to receive(:follow_accepted)
+        following.accept!
+
+        expect(follower.entity).to have_received(:follow_accepted).with(following).once
       end
     end
 
     context 'when following a remote actor' do
-      let(:local_user) { FactoryBot.create :user }
-      let(:remote_actor) { FactoryBot.create :distant_actor }
+      let(:follower) { FactoryBot.create :local_actor }
+      let(:target) { FactoryBot.create :distant_actor }
+      let(:following) { described_class.build actor: follower, target_actor: target }
 
       it 'creates Follow activity' do # rubocop:disable RSpec/ExampleLength
         allow(Activity).to receive(:create!)
-        described_class.create actor: local_user.federails_actor, target_actor: remote_actor
+        following.save!
         expect(Activity).to have_received(:create!).with(
           action: 'Follow',
-          actor:  local_user.federails_actor,
-          entity: remote_actor
+          actor:  follower,
+          entity: target
         )
       end
 
       it 'queues NotifyInboxJob' do
         expect do
-          described_class.create actor: local_user.federails_actor, target_actor: remote_actor
+          following.save!
         end.to have_enqueued_job(NotifyInboxJob).once
       end
     end
 
-    context 'when unfollowing a distant actor' do
-      let(:local_user) { FactoryBot.create :user }
-      let(:distant_actor) { FactoryBot.create :distant_actor }
-      let!(:follow) { described_class.create actor: local_user.federails_actor, target_actor: distant_actor }
+    context 'when unfollowing a local actor' do
+      let(:follower) { FactoryBot.create :local_actor }
+      let(:target) { FactoryBot.create :local_actor }
+      let!(:following) { described_class.create! actor: follower, target_actor: target }
 
       it 'creates Undo activity when Following is destroyed' do # rubocop:disable RSpec/ExampleLength
         allow(Activity).to receive(:create!)
-        follow.destroy
+        following.destroy!
         expect(Activity).to have_received(:create!).with(
           action: 'Undo',
-          actor:  local_user.federails_actor,
-          entity: Activity.find_by(action: 'Follow', actor: local_user.federails_actor, entity: distant_actor)
+          actor:  follower,
+          entity: Activity.find_by(action: 'Follow', actor: follower, entity: target)
         )
       end
 
       it 'queues NotifyInboxJob' do
-        expect { follow.destroy }.to have_enqueued_job(NotifyInboxJob).once
+        expect { following.destroy! }.to have_enqueued_job(NotifyInboxJob).once
+      end
+    end
+
+    context 'when unfollowing a distant actor' do
+      let(:follower) { FactoryBot.create :local_actor }
+      let(:target) { FactoryBot.create :distant_actor }
+
+      let!(:following) { described_class.create! actor: follower, target_actor: target }
+
+      it 'creates Undo activity when Following is destroyed' do # rubocop:disable RSpec/ExampleLength
+        allow(Activity).to receive(:create!)
+        following.destroy!
+        expect(Activity).to have_received(:create!).with(
+          action: 'Undo',
+          actor:  follower,
+          entity: Activity.find_by(action: 'Follow', actor: follower, entity: target)
+        )
+      end
+
+      it 'queues NotifyInboxJob' do
+        expect { following.destroy! }.to have_enqueued_job(NotifyInboxJob).once
       end
     end
 
     context 'when a remote actor follows a local user' do
-      let(:local_user) { FactoryBot.create :user }
-      let(:remote_actor) { FactoryBot.create :distant_actor }
+      let(:follower) { FactoryBot.create :distant_actor }
+      let(:target) { FactoryBot.create :local_actor }
+
+      let(:following) { described_class.build actor: follower, target_actor: target }
 
       it 'does not create Follow activity' do
         expect do
-          described_class.create actor: remote_actor, target_actor: local_user.federails_actor
+          following.save!
         end.not_to change(Activity, :count)
       end
 
       it 'does not queue NotifyInboxJob' do
         expect do
-          described_class.create actor: remote_actor, target_actor: local_user.federails_actor
+          following.save!
         end.not_to have_enqueued_job(NotifyInboxJob)
       end
     end
 
     context 'when a remote actor unfollows a local user' do
-      let(:local_user) { FactoryBot.create :user }
-      let(:remote_actor) { FactoryBot.create :distant_actor }
-      let!(:follow) { described_class.create actor: remote_actor, target_actor: local_user.federails_actor }
+      let(:follower) { FactoryBot.create :distant_actor }
+      let(:target) { FactoryBot.create :local_actor }
+      let!(:following) { described_class.create! actor: follower, target_actor: target }
 
       it 'does not create Follow activity' do
-        expect { follow.destroy }.not_to change(Activity, :count)
+        expect { following.destroy! }.not_to change(Activity, :count)
       end
 
       it 'does not queue NotifyInboxJob' do
-        expect { follow.destroy }.not_to have_enqueued_job(NotifyInboxJob)
+        expect { following.destroy! }.not_to have_enqueued_job(NotifyInboxJob)
       end
     end
   end
