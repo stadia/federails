7202ca6a0acb28dc6a4e63788aca401c14c51c40
Manuel Tancoigne <manu@experimentslabs.com>
2025-04-06 21:57:48 +0200

    feat(models): Create concern to handle Delete requests

---
+++ app/models/concerns/federails/handles_delete_requests.rb
@@ -0,0 +1,31 @@
+module Federails
+  # Model concern providing hooks for on_federails_delete_requested callback
+  #
+  # ```rb
+  # # Example migration
+  # add_column :my_table, :uuid, :text, default: nil, index: { unique: true }
+  # ```
+  #
+  # Usage:
+  #
+  # ```rb
+  # class MyModel < ApplicationRecord
+  #   include Federails::HandlesDeleteRequests
+  #
+  #   on_federails_delete_requested -> { delete! }
+  # end
+  module HandlesDeleteRequests
+    extend ActiveSupport::Concern
+
+    # Class methods automatically included in the concern.
+    module ClassMethods
+      def on_federails_delete_requested(*args)
+        set_callback :on_federails_delete_requested, *args
+      end
+    end
+
+    included do
+      define_callbacks :on_federails_delete_requested
+    end
+  end
+end
