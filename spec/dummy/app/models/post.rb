ebe26db760dc47ab4f7ee63984c1feb47557da4b
Manuel Tancoigne <manu@experimentslabs.com>
2025-04-07 09:08:03 +0200

    feat(dummy): Add soft delete support on posts

--- spec/dummy/app/models/post.rb
+++ spec/dummy/app/models/post.rb
@@ -1,8 +1,11 @@
 class Post < ApplicationRecord
   include Federails::DataEntity
+  include FederatedAndSoftDeletable
 
-  acts_as_federails_data handles:             'Note',
-                         actor_entity_method: :user
+  acts_as_federails_data handles:                 'Note',
+                         actor_entity_method:     :user,
+                         soft_deleted_method:     :soft_deleted?,
+                         soft_delete_date_method: :deleted_at?
 
   validates :title, presence: true, allow_blank: false
   validates :content, presence: true, allow_blank: false
