256915bcf7ea0675c1b4dac64c22a855ccf3bf1c
Manuel Tancoigne <manu@experimentslabs.com>
2025-05-19 11:04:21 +0200

    feat(dummy): Specify "profile_url_method" for User

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   include Federails::ActorEntity
 
-  acts_as_federails_actor username_field:    :id,
-                          name_field:        :email,
-                          user_count_method: :user_count
+  acts_as_federails_actor username_field:     :id,
+                          name_field:         :email,
+                          profile_url_method: :user_url,
+                          user_count_method:  :user_count
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
