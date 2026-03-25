29fbccc6a26c81aeffd7580ab2552fac694c0c4e
James Smith <james@floppy.org.uk>
2026-03-25 15:12:47 +0000

    Queue jobs on specified queue

--- app/jobs/federails/notify_inbox_job.rb
+++ app/jobs/federails/notify_inbox_job.rb
@@ -2,8 +2,6 @@ require 'fediverse/notifier'
 
 module Federails
   class NotifyInboxJob < ApplicationJob
-    queue_as :default
-
     def perform(activity)
       activity.reload
       Fediverse::Notifier.post_to_inboxes(activity)
