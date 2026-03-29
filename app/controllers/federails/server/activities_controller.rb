fe101e75a0961c882164a62cf3a1c96e47abbd1f
jeff.dean <stadia@gmail.com>
2026-03-29 16:22:07 +0900

    RBS 인라인 타입 시그니처 추가 (컨트롤러)
    Federails 컨트롤러 파일에 `# rbs_inline: enabled` 주석을 추가하고,
    RBS::Inline으로 생성된 `.rbs` 시그니처 파일을 `sig/generated/` 경로에
    추가한다.

--- app/controllers/federails/server/activities_controller.rb
+++ app/controllers/federails/server/activities_controller.rb
@@ -1,3 +1,5 @@
+# rbs_inline: enabled
+
 require 'fediverse/inbox'
 
 module Federails
--- app/controllers/federails/server_controller.rb
+++ app/controllers/federails/server_controller.rb
@@ -1,3 +1,5 @@
+# rbs_inline: enabled
+
 module Federails
   class ServerController < ::ActionController::Base # rubocop:disable Rails/ApplicationController
     include Pagy::Method
