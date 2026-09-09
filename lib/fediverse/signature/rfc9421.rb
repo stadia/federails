7076c31fd95e1268d920ac3d5f1668b2c936942a
James Smith <james@floppy.org.uk>
2026-09-09 14:40:59 +0100

    Generate Content-Digest header

--- lib/fediverse/signature/rfc9421.rb
+++ lib/fediverse/signature/rfc9421.rb
@@ -3,11 +3,20 @@ module Fediverse
     class Rfc9421
       class << self
         def sign(sender:, request:)
+          request.headers['Content-Digest'] = digest(request.body)
           request
         end
 
         def verify(sender:, request:)
           false
+        end
+
+        private
+
+        def digest(message)
+          "sha-256=:#{Base64.strict_encode64(
+            OpenSSL::Digest.new('SHA256').digest(message)
+          )}:"
         end
       end
     end
--- spec/lib/fediverse/signature/rfc9421_spec.rb
+++ spec/lib/fediverse/signature/rfc9421_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 require 'fediverse/signature/rfc9421'
 
 RSpec.describe Fediverse::Signature::Rfc9421 do
+  let(:actor) { FactoryBot.create(:user).fedipub_actor }
+
+  context 'when signing requests' do # rubocop:todo RSpec/MultipleMemoizedHelpers
+    let(:request) do
+      Faraday.default_connection.build_request(:post) do |req|
+        req.url '/inbox'
+        req.body = 'test'
+        req.headers['Host'] = 'example.com'
+        req.headers['Date'] = Time.now.utc.httpdate
+      end
+    end
+    let(:signed_request) { described_class.sign(sender: actor, request: request) }
+    let(:signature_input) { signed_request.headers['Signature-Input'] }
+    let(:signature) { signed_request.headers['Signature'] }
+    let(:signature_parts) { signature.split(',') }
+
+    it 'adds Content-Digest header to request' do
+      expect(signed_request.headers['Content-Digest']).to eq 'sha-256=:n4bQgYhMfWWaL+qgxVrQFaO/TxsrC4Is0V1sFbDwCgg=:'
+    end
+  end
 end
